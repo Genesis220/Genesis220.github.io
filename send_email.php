32b347bd73a0d95fe8bad26009c47bcb352afce3
Nathan Lau <130193002+Genesis220@users.noreply.github.com>
2024-09-10 22:25:34 -0400

    Add files via upload

---
+++ send_email.php
@@ -0,0 +1,18 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $message = htmlspecialchars($_POST['message']);
+
+    $to = "nathanlau2008@gmail.com";
+    $subject = "New Contact Form Message from $name";
+    $body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Message successfully sent!";
+    } else {
+        echo "Message delivery failed...";
+    }
+}
+?>
